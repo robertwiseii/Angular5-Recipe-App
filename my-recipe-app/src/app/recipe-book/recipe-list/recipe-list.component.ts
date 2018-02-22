de341238b9176f69758ae04ea960beaee37fde47
Bobby Wise <robert.b.wiseii@gmail.com>
2018-02-22 01:03:00 -0700

    Completed Course Project - Components & Databinding

--- my-recipe-app/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ my-recipe-app/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,15 +7,21 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe("A Test Recipe", "This is simply a test", "https://c1.staticflickr.com/5/4091/4836139726_fdf79e1e6b_b.jpg"),
-    new Recipe("A Test Recipe", "This is simply a test", "https://c1.staticflickr.com/5/4091/4836139726_fdf79e1e6b_b.jpg"),
-    new Recipe("A Test Recipe", "This is simply a test", "https://c1.staticflickr.com/5/4091/4836139726_fdf79e1e6b_b.jpg")
+    new Recipe("Fish Tacos", "What's better than tacos and fish?", "https://c1.staticflickr.com/5/4091/4836139726_fdf79e1e6b_b.jpg"),
+    new Recipe("BBQ Ribs", "Mmmmmmm... Get in my Belly!", "https://c1.staticflickr.com/4/3067/2855239227_a267e71cb8_b.jpg"),
+    new Recipe("Shrip Salad", "This is no small salad.", "https://static.pexels.com/photos/51948/gourmet-salad-lunch-breakfast-51948.jpeg")
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
